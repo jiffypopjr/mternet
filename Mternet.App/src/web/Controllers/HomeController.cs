3d8cb1106fc6290bedee8e2fe5015eaa49916929
Joshua Smith <joshua.smith@yahoo.com>
2016-04-11 20:21:33 -0500

    Initial web check-in

---
+++ Mternet.App/src/web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Mternet, Inc.";
+
+            return View();
+        }
+
+       
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
